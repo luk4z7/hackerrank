a2e117f80a648fdc418f6eaa509986bc8483425b
Lucas Alves <luk4z_7@hotmail.com>
2021-09-07 18:25:59 -0300

    Change the structure of the bubblesort folder

--- bubblesort/main.go
+++ bubblesort/main.go
@@ -1,6 +1,20 @@
 package main
 
-func bubbleSort(data []int32) []int32 {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'countSwaps' function below.
+ *
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+func countSwaps(data []int32) {
 	var swaps int64
 
 	for i := range data {
@@ -20,7 +34,43 @@ func bubbleSort(data []int32) []int32 {
 		}
 	}
 
-	p("swaps: ", swaps, "first: ", data[0], "last: ", data[len(data)-1])
+	fmt.Printf("Array is sorted in %d swaps.\n", swaps)
+	fmt.Printf("First Element: %d\n", data[0])
+	fmt.Printf("Last Element: %d\n", data[len(data)-1])
+}
 
-	return data
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	countSwaps(a)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
